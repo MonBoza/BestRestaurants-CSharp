c9ba7a159aa6a7ab1a6d785f10780328d5b027e0
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-31 09:34:07 -0800

    Create models
    Co-authored-by: Monica Barboza <MonBoza@gmail.com>

---
+++ BestRestaurants/Models/Restaurant.cs
@@ -0,0 +1,11 @@
+namespace BestRestaurants.Models
+{
+  public class Restaurant
+  {
+  public int RestaurantId { get; set;}
+  public string RestaurantName { get; set; }
+  public string Description { get; set; }
+  public int CuisineId { get; set; }
+  public Cuisine Cuisine { get; set; }
+  }
+}
